60cb117a90edb303fa654744ed16306f2b855a07
Chittra <chittra.kumari_cs.ccv19@gla.ac.in>
2021-05-14 13:51:59 +0530

    Custom Classes & Named Parameters

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 
 
   import 'package:flutter/material.dart';
+import 'quote.dart';
 
   void main() => runApp(MaterialApp(
 
@@ -15,10 +16,10 @@
   }
 
   class _QuoteListState extends State<QuoteList> {
-    List<String> quotes=[
-      'Be yourself; everyone else is already taken',
-      'I have nothing to declare except my genius',
-      'The truth is rarely pure and never simple'
+    List<Quote> quotes=[
+      Quote(text:'Be yourself; everyone else is already taken',author:'Oscar Wilde' ),
+      Quote(text:'I have nothing to declare except my genius',author:'Oscar Wilde'),
+      Quote(text:'The truth is rarely pure and never simple',author: 'Oscar Wilde'),
     ];
     Widget build(BuildContext context) {
 
@@ -48,7 +49,7 @@
 
 
         quotes.map((quote){
-      return Text(quote);
+      return Text('${quote.text} -  ${quote.author}');
       }
       ).toList(),
 
